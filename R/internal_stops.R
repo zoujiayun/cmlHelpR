d120d8202ec61655ba49b5e0a6743d7bf42ca935
Alastair <alastair.j.lud@gmail.com>
2019-04-17 14:03:35 +0930

    Adapted to use output file rather than log file. Also adapted the script to filter for sites you want to keep rather than want to get rid of

--- R/internal_stops.R
+++ R/internal_stops.R
@@ -8,32 +8,36 @@
 #' @keywords stop, column, converted
 #' @export
 #' @examples
-#' internal_stops(dir_path = "path/to/codeml_out", model_string = "Model2Selection", tree_string = "brownForeground", nSites = 2)
-internal_stops <- function(dir_path, model_string, tree_string, nSites = NULL) {
+#' internal_stops(dir_path = "path/to/codeml_out", tree_string = "brownForeground", nSites = 2)
+internal_stops <- function(dir_path, tree_string, nSites = NULL) {
 
-  fl <- list.files(path = dir_path, pattern = ".log", full.names = TRUE, recursive = TRUE)
-  fl <- fl[str_detect(string = fl, pattern = model_string)]
+  fl <- list.files(path = dir_path, pattern = ".output", full.names = TRUE, recursive = TRUE)
   fl <- fl[str_detect(string = fl, pattern = tree_string)]
-  fl <- set_names(x = fl, value = gsub(".+/(.*)/(.*)/codeml.log", "\\1;\\2", fl))
+  fl <- magrittr::set_names(x = fl, value = basename(fl))
   lst.fl <- map(fl, read_lines)
 
   ## Parsing number of columns converted to '???'
   lst.nCols <- map(names(lst.fl), ~{
-    vec.line <- lst.fl[[.x]][grep(pattern = "columns are converted into", x = lst.fl[[.x]], perl = TRUE)]
-    int.nCols <- as.numeric(str_remove(string = vec.line, pattern = "columns.*"))
+
+    s <- grep(pattern = "Before deleting alignment gaps", x = lst.fl[[.x]]) + 3
+
+    v <- lst.fl[[.x]][s]
+    c <- stringr::str_count(string = v, pattern = "\\?\\?\\?")
 
     tibble(gene_tree = .x,
-           nCols = int.nCols)
+           nCols = c)
   })
 
   df.nCols <-  bind_rows(lst.nCols)
-  df.nCols <- separate(df.nCols, gene_tree, c("gene", "tree"), ";")
+  df.nCols <- separate(df.nCols, gene_tree, c("gene", "tree"), "::")
 
   if (!is.null(nSites)) {
-    df.filt <- filter(df.nCols, nCols >= nSites)
+    df.filt <- filter(df.nCols, nCols <= nSites)
     return(df.filt)
   } else {
     return(df.nCols)
   }
 
 }
+
+t <- internal_stops(dir_path = "~/Documents/projects/Kate_general/analyses/1901_RBH/04_codeml/M2a", tree_string = "brownForeground", nSites = 1)
