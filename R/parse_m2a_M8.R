4f95a7b02ed86e7b82a967f3acd72f70f17ff8e1
Alastair <alastair.j.lud@gmail.com>
2019-06-25 10:07:34 +0930

    Readability and suppression of warnings for trivial tasks (seprations/coercion of "NA" to NA etc...)

--- R/parse_m2a_M8.R
+++ R/parse_m2a_M8.R
@@ -1,12 +1,12 @@
 #' Parse BEB output from M2a and M8 codeml output
 #'
 #' General run down of what it does
-#' @param lst List of codeml output files
+#' @param lines List of codeml output files
 #' @param c number of cores
 #' @keywords internal
-.parse_m2a_M8 <- function(lst, c) {
+.parse_m2a_M8 <- function(lines, c) {
 
-  obj <- lapply(lst, function(y) {
+  obj <- lapply(lines, function(y) {
 
     s <- grep(pattern = "Bayes Empirical Bayes", x = y)
     e <- grep(pattern = "^The grid", x = y)
@@ -31,10 +31,10 @@
   df <- tidyr::separate(data = df, col = value, into = c("pos", "aa", "val", "postMean", "plusMinus", "SE"), sep = "_")
   df <- dplyr::mutate(.data = df,
                      signif = stringr::str_extract(string = val, pattern = "\\*+"),
-                     val = as.numeric(stringr::str_remove_all(string = val, pattern = "\\*")),
+                     val = suppressWarnings(as.numeric(stringr::str_remove_all(string = val, pattern = "\\*"))),
                      pval = 1 - val,
                      aa = dplyr::na_if(x = aa, "NA"))
-  df <- dplyr::mutate_at(.tbl = df, .vars = c("pos", "postMean", "SE"), .funs = as.numeric)
+  df <- suppressWarnings(dplyr::mutate_at(.tbl = df, .vars = c("pos", "postMean", "SE"), .funs = as.numeric))
   df <- tidyr::separate(data = df, col = id, into = c("gene", "tree"), sep = "_")
   df <- select(.data = df, gene, tree, pos, aa, val, pval, signif, postMean, SE)
 
