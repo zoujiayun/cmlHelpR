98bb31b157f4903b3c7c7cb8d0a222cfb503d5f0
Alastair <alastair.j.lud@gmail.com>
2019-06-25 15:10:56 +0930

    Improved method to obtain files + general updates

--- R/getBranchdNdS.R
+++ R/getBranchdNdS.R
@@ -2,21 +2,29 @@
 #'
 #' Given a list of files, this function will parse the branch dN/dS value from the
 #' CODEML output. It returns a list and nested dataframe object of dN/dS values for each file.
-#' @param file_list Vector of file names with path
+#' @param directory_path Vector of file names with path
 #' @keywords helper
 #' @export
 #' @examples
-#' getBranchdNdS(file_list = list.files(path = "path/to/files"))
-getBranchDNDS <- function(file_list) {
+#' getBranchdNdS(directory_path = list.files(path = "path/to/codeml/outDir", models = c("M1a", "M8")))
+getBranchDNDS <- function(directory_path, models) {
 
-  ## Accepted models
+  ## Check: do the models provided match all contain branch dN/dS values
   g <- c("FreeRatio" ,"M0" ,"M1a" ,"M2a_Rel" ,"M3" ,"M7" ,"M8" , "TwoRatio")
-  g <- paste0("/", g, "/", collapse = "|")
+  if(!all(is.element(models, g))){
+    stop(call. = TRUE, "One of the specified models will not contain branch dN/dS values")
+  }
+  m <- paste0("/", models, collapse = "|")
 
-  ## Filter out models not in g
-  f <- file_list[stringr::str_detect(string = file_list, pattern = g)]
+  ## Listing all directories in directory path
+  d <- list.dirs(path = directory_path, full.names = TRUE, recursive = TRUE)
+  d <- d[stringr::str_detect(string = d, pattern = m)]
+
+  ## Listing files + reading them in
+  f <- purrr::map(d, list.files, pattern = ".out", full.names = TRUE)
+  f <- unlist(x = f)
   f <- magrittr::set_names(x = f, value = stringr::str_remove(string = sub(".*/(.*)/(.*)$", "\\1_\\2", f), pattern = ".out"))
-  f <- purrr::map(f, read_lines)
+  f <- purrr::map(f, readr::read_lines)
 
   ## Iterate through each file and get values
   d <- purrr::map(f, ~{
