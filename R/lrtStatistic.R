6f752a1247e6f59b6bad8dbea09006a663b723e0
Alastair <alastair.j.lud@gmail.com>
2019-07-01 12:14:16 +0930

    Re-worked methods to deal with base-tree null/site models

--- R/lrtStatistic.R
+++ R/lrtStatistic.R
@@ -3,8 +3,7 @@
 #' General run down of what it does
 #' @param dir_path Path to codeml_parallel output directory.
 #' @param lst_comparisons Combinations/comparisons of the above models
-#' @param ext Extension of CODEML output files
-#' @keywords Likelihood ratio test, LRT
+#' @param ext Extension of CODEML output files. Defaults to ".out"
 #' @export
 #' @examples
 #' lrt_statistic(dir_path = "path/to/codeml_out", models = c("M1a", "M2a"), lst_comparisons = c("M1a", "M2a"))
@@ -13,6 +12,10 @@ lrtStatistic <- function(dir_path, lst_comparisons, ext = ".out") {
   ## Models
   m <- unlist(lst_comparisons)
   m <- unique(m)
+
+  ## Predefined groupings
+  nsm <- c("M0", "M1a", "M2a", "M3", "M7", "M8")
+  bsm <- c("FreeRatio", "TwoRatio", "ModelA", "ModelA1", "M2a_Rel", "CmC", "CmD")
 
   ## Selecting output dirs for analysis
   d <- list.dirs(path = dir_path, full.names = TRUE)
@@ -35,27 +38,30 @@ lrtStatistic <- function(dir_path, lst_comparisons, ext = ".out") {
   np_lnL <- purrr::map(f, .parse_np_lnL)
   np_lnL <- dplyr::bind_rows(np_lnL, .id = "model")
 
-  ## Long to wide format on multiple variables
-  np_lnL <- data.table::dcast(data.table::setDT(np_lnL), gene_tree ~ model, value.var = c("np", "lnL"))
-  np_lnL <- tibble::as_tibble(x = np_lnL)
-  np_lnL <- tidyr::separate(data = np_lnL,
-                            col = gene_tree,
-                            into = c("gene","tree"),
-                            sep = "_")
+  ## Calculating LRT statistic
+  out_lrt <- purrr::map(lst_comparisons, ~{
 
-  ## Model comparisons
-  print("Generating list comparisons")
-  comp <- purrr::map(lst_comparisons, ~{
-    tmp <- dplyr::select(.data = np_lnL, gene, tree, dplyr::matches(paste(.x, "$", collapse = "|", sep = "")))
-    tmp <- dplyr::mutate(.data = tmp,
-                         delta = (2*(abs(tmp[[6]] - tmp[[5]]))),
-                         df = abs(tmp[[4]] - tmp[[3]]),
-                         pval = pchisq(delta, df, lower.tail=FALSE))
+    if(all(is.element(.x, nsm))) {       ## Both models are null/site models
+
+      df <- .lrt_nsm(df = np_lnL, mdl = .x)
+      return(df)
+
+    } else if(all(is.element(.x, bsm))){ ## Both models are branch/branch-site/clade models
+
+      df <- .lrt_bcm(df = np_lnL, mdl = .x)
+      return(df)
+
+    } else {                             ## One model from each camp
+
+      df <- .lrt_hnh(df = np_lnL, mdl = .x)
+      return(df)
+
+    }
 
   })
 
   ## Assigning names to list object
-  names(comp) <- purrr::map(lst_comparisons, ~{paste(.x, collapse = "_")})
-  return(comp)
+  names(out_lrt) <- purrr::map(lst_comparisons, ~{paste(.x, collapse = "-")})
+  return(out_lrt)
 
 }
