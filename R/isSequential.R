907c86218abdd79837d24798c055fc42ba534c93
Alastair <alastair.j.lud@gmail.com>
2019-07-05 15:15:16 +0930

    Corrected method to actually extract the sites it actually should...

--- R/isSequential.R
+++ R/isSequential.R
@@ -16,27 +16,56 @@ isSequential <- function(beb_long, gap_threshold){
   nst <- dplyr::group_by(.data = beb_long, model, gene, tree)
   nst <- tidyr::nest(data = nst)
 
-  ## New column with sites of interest
+  ## Sites that fall within proximity threshold (close to other sites under slection)
   nst <- dplyr::mutate(.data = nst,
                        within_threshold = purrr::map(data, ~{
 
-                         pos <- .x[["pos"]]
-                         pos_gap <- which(diff(pos) <= gap_threshold)
-                         pos_len <- pos_len <- length(pos_gap)
+                         pos <- .x[["pos"]] ## Positions
+                         rn <- 1:nrow(.x) ## Row number used as position name
+                         names(pos) <- rn
+
+                         pos_gap <- which(diff(pos) <= gap_threshold) ## Sites within the proximity
+                         pos_len <- length(pos_gap) ## N-sites that fail proximity threshold
 
                          if(pos_len > 0){ ## When there is at least one site that is too close to another
-                           idx <- pos_gap + 1
-                           idx <- sort(unique(c(pos_gap, idx)))
-                           .x[idx, ]
+                           gap_idx <- as.numeric(names(pos_gap))
+                           idx <- gap_idx - 1 ## staggered id now (matches gap between vector values) - need to subtract 1 to align ids back to actual rows
+                           idx <- sort(unique(c(gap_idx, idx))) ## Joining vectors to get all sites to extract
+
+                           o <- .x[1:nrow(.x) %in% idx, ] ## Subsetting dataframe for sites that are within proximity threshold
+                           return(o)
                          } else {
                            return(NULL)
                          }
-                       })
-  )
+                       }))
+
+  ## Sites outside of proximity distance (dispersed)
+  nst <- dplyr::mutate(.data = nst,
+                       confident_sites = purrr::map(data, ~{
+
+                         pos <- .x[["pos"]]
+                         rn <- 1:nrow(.x)
+                         names(pos) <- rn
+
+                         pos_gap <- which(diff(pos) <= gap_threshold)
+                         pos_len <- length(pos_gap)
+
+                         if(pos_len > 0){
+                           gap_idx <- as.numeric(names(pos_gap))
+                           idx <- gap_idx - 1
+                           idx <- sort(unique(c(gap_idx, idx)))
+
+                           o <- .x[!1:nrow(.x) %in% idx, ]
+                           return(o)
+                         } else {
+                           .x
+                         }
+                       }))
 
   ## Naming output
   nm <- paste(nst[["model"]], nst[["gene"]], nst[["tree"]], sep = "_")
   names(nst[["within_threshold"]]) <- nm
+  names(nst[["confident_sites"]]) <- nm
 
   return(nst)
 
