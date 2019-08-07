a727693a2c783b9e85519f4a864967452c9997c8
Alastair <alastair.j.lud@gmail.com>
2019-08-07 13:39:40 +0930

    Global variable correction

--- R/lrt_nsm.R
+++ R/lrt_nsm.R
@@ -4,18 +4,19 @@
 #' @param df Long form dataframe
 #' @param mdl Vector of two null/site models for comparison
 #' @keywords internal
+#' @importFrom rlang .data
 #' @export
 .lrt_nsm <- function(df, mdl){
 
   ## Parsing input dataframe
-  o <- dplyr::filter(.data = df, stringr::str_detect(string = model, pattern = paste0(mdl, "$", collapse = "|")))
+  o <- dplyr::filter(.data = df, stringr::str_detect(string = .data$model, pattern = paste0(mdl, "$", collapse = "|")))
   o <- tibble::as_tibble(data.table::dcast(data.table::setDT(o), id ~ model, value.var = c("np", "lnL")))
 
   ## LRT calculation
   o <- dplyr::mutate(.data = o,
                       delta = (2*(abs(o[[5]] - o[[4]]))),
                       degFree = abs(o[[3]] - o[[2]]),
-                      pval = pchisq(delta, degFree, lower.tail=FALSE))
+                      pval = stats::pchisq(.data$delta, .data$degFree, lower.tail=FALSE))
 
   return(o)
 }
