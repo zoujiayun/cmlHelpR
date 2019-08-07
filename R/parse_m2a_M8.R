8990136256eb818b7dabc2b1ff160b6664d5a091
Alastair <alastair.j.lud@gmail.com>
2019-08-07 13:58:08 +0930

    Binding for global variables

--- R/parse_m2a_M8.R
+++ R/parse_m2a_M8.R
@@ -5,6 +5,7 @@
 #' @param lines List of codeml output files
 #' @param c number of cores
 #' @keywords internal
+#' @importFrom rlang .data
 .parse_m2a_M8 <- function(lines, c) {
 
   obj <- parallel::mclapply(lines, mc.cores = c, function(y) {
@@ -27,18 +28,18 @@
 
   df <- purrr::map(.x = obj, .f = tibble::enframe, name = NULL)
   df <- dplyr::bind_rows(df, .id = "id")
-  df <- dplyr::mutate(.data = df, value = stringr::str_replace_all(string = value, pattern = "\\s+", replacement = "_"))
-  df <- dplyr::filter(.data = df, value != "")
-  df <- suppressWarnings(tidyr::separate(data = df, col = value, into = c("pos", "aa", "val", "postMean", "plusMinus", "SE"), sep = "_"))
+  df <- dplyr::mutate(.data = df, value = stringr::str_replace_all(string = .data$value, pattern = "\\s+", replacement = "_"))
+  df <- dplyr::filter(.data = df, .data$value != "")
+  df <- suppressWarnings(tidyr::separate(data = df, col = .data$value, into = c("pos", "aa", "val", "postMean", "plusMinus", "SE"), sep = "_"))
   df <- dplyr::mutate(.data = df,
-                      signif = stringr::str_extract(string = val, pattern = "\\*+"),
-                      val = suppressWarnings(as.numeric(stringr::str_remove_all(string = val, pattern = "\\*"))),
-                      pval = 1 - val,
-                      aa = dplyr::na_if(x = aa, "NA"))
+                      signif = stringr::str_extract(string = .data$val, pattern = "\\*+"),
+                      val = suppressWarnings(as.numeric(stringr::str_remove_all(string = .data$val, pattern = "\\*"))),
+                      pval = 1 - .data$val,
+                      aa = dplyr::na_if(x = .data$aa, "NA"))
   df <- suppressWarnings(dplyr::mutate_at(.tbl = df, .vars = c("pos", "postMean", "SE"), .funs = as.numeric))
   # df <- tidyr::separate(data = df, col = id, into = c("gene", "tree"), sep = "_")
   # df <- dplyr::select(.data = df, gene, tree, pos, aa, val, pval, signif, postMean, SE)
-  df <- dplyr::select(.data = df, id, pos, aa, val, pval, signif, postMean, SE)
+  df <- dplyr::select(.data = df, .data$id, .data$pos, .data$aa, .data$val, .data$pval, .data$signif, .data$postMean, .data$SE)
 
   return(df)
 
